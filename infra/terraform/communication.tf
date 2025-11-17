9e41e905bbcf21fdc44d674a769484f104202fa5
Jin Lee (HLS US SE) <jin.lee+microsoft@microsoft.com>
2025-11-16 19:14:35 -0600

    refactor: Comment out unused Azure email communication service resources

--- infra/terraform/communication.tf
+++ infra/terraform/communication.tf
@@ -1,12 +1,12 @@
 # ============================================================================
 # AZURE COMMUNICATION SERVICES EMAIL
 # ============================================================================
-resource "azurerm_email_communication_service" "main" {
-  name                = local.resource_names.email_service
-  resource_group_name = azurerm_resource_group.main.name
-  data_location       = var.acs_data_location
-  tags                = local.tags
-}
+# resource "azurerm_email_communication_service" "main" {
+#   name                = local.resource_names.email_service
+#   resource_group_name = azurerm_resource_group.main.name
+#   data_location       = var.acs_data_location
+#   tags                = local.tags
+# }
 
 # resource "azurerm_email_communication_service_domain" "managed" {
 #   name                             = local.resource_names.email_domain
